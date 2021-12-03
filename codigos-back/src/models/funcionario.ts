16816aaa61da5c5a43f4791adc78fe58e6dcb107
belamarib <55455937+belamarib@users.noreply.github.com>
2021-12-03 16:04:22 -0300

    criação da pasta com os códigos de backend

---
+++ codigos-back/src/models/funcionario.ts
@@ -0,0 +1,15 @@
+export class Funcionario {
+    id: number;
+    nome: string;
+    email: string;
+    cpf: string;
+    senha: string;
+    
+    constructor (id: number, nome: string, email: string, cpf: string, senha: string) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+}
--- codigos/src/app/pagina-login/pagina-login.component.ts
+++ codigos/src/app/pagina-login/pagina-login.component.ts
@@ -5,11 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pagina-login.component.html',
   styleUrls: ['./pagina-login.component.css']
 })
-export class PaginaLoginComponent implements OnInit {
 
-  constructor() { }
+export class PaginaLoginComponent implements OnInit {
+  nome: string;
+  nomes: string[];
+  senha: string;
+  senhas: string[];
+
+
+  constructor() {
+    this.nome = "";
+    this.senha = "";
+    this.nomes = [];
+    this.senhas = [];
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  salvarNome() {
+    if (this.nome.length >= 3) {
+      this.nomes.push(this.nome);
+      this.nome = "";
+    }
+  }
+
+  salvarSenha() {
+    if (this.senha.length >= 3) {
+      this.senhas.push(this.senha);
+      this.senha = "";
+    }
   }
 
 }
+
+
