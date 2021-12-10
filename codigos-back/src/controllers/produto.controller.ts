94f29f085e2df3e63c34641e3cafb9b3cba13589
Amanda <aag@cin.ufpe.br>
2021-12-10 00:04:36 -0300

    backend crud de produtos

--- codigos-back/src/controllers/produto.controller.ts
+++ codigos-back/src/controllers/produto.controller.ts
@@ -13,23 +13,58 @@ export class ProdutoController {
         return this.produtos;
     }
 
-    createProduto(nome : string, preco: number, quantidade: number, validade: Date) : boolean {
+    getProdutoByNome(nomebusca : string) : Produto {
+        const produto = this.produtos.find(u => u.nome == nomebusca);
+        return produto;
+    }
+
+    createProduto(nome : string, preco: number, quantidade: number, validade: Date) : number {
         if (this.produtos.find(u => u.nome == nome)) {
-            return false; //nomes iguais
+            return 1; //nomes iguais
         } else {
             let dataAtual = new Date();
-            if (validade.getFullYear() < dataAtual.getFullYear()) {
-                return false;
-            } else if (validade.getFullYear() > dataAtual.getFullYear()) {
+            if (validade < dataAtual) {
+                return 2; //data de validade inválida
+            } else {
                 let newproduto = new Produto(this.count, nome, preco, quantidade, validade);
                 this.produtos.push(newproduto);
                 this.count ++;
-                return true;
-            } else { //mesmo ano
-
+                return 0; //ok
             }
         }
     }
+
+    updateProduto(nome : string, preco : number, quantidade: number, validade: Date) : number {
+        let produtoIndex = this.produtos.findIndex(u => u.nome == nome);
+
+        if (produtoIndex == -1) {
+            return 1; //usuario nao encontrado
+        }
+
+        let dataAtual = new Date();
+        if (validade < dataAtual) {
+            return 2; //data de validade invalida
+        } else {
+            this.produtos[produtoIndex].preco = preco;
+            this.produtos[produtoIndex].quantidade = quantidade;
+            this.produtos[produtoIndex].validade = validade;
+            return 0; //ok
+        }
+    }
+
+    deleteProduto(nome : string) : boolean {
+        let produtoIndex = this.produtos.findIndex(u => u.nome == nome);
+
+        if (produtoIndex == -1) {
+            return false; //usuario nao encontrado
+        }
+
+        this.produtos.splice(produtoIndex, 1);
+        return true;
+
+    }
+
+
 
     
 }
--- codigos-back/src/routes/produto.routes.ts
+++ codigos-back/src/routes/produto.routes.ts
@@ -22,19 +22,77 @@ produtoRouter.route("/")
 
         const newproduto = produtoController.createProduto(nome, preco, quantidade, validade);
 
-        if (newproduto) {
-            return res.json({Message: "Produto cadastrado com sucesso!"});
-        } else {
-            return res.status(409).json({Message: "Erro. Já existe um produto com esse nome."});
-        }
+        switch(newproduto) {
+            case 1: { 
+                return res.status(409).json({Message: "Erro. Já existe um produto com esse nome."});
+            } 
+            case 2: { 
+                return res.status(409).json({Message: "Erro. Data de validade inválida."});
+            } 
+            default: { 
+                return res.json({Message: "Produto cadastrado com sucesso!"});
+            } 
+        } 
+         
     })
 
     .put((req: Request, res: Response) => {
-        return res.json({Message: "PUT recebido com sucesso na rota /produtos"});
+        return res.json({Warning: "Método PUT não suportado para a rota /users"});
     })
 
     .delete((req: Request, res: Response) => {
-        return res.json({Message: "DELETE recebido com sucesso na rota /produtos"});
+        return res.json({Warning: "Método DELETE não suportado para a rota /users"});
     });
+
+produtoRouter.route("/:nome")
+    .get((req: Request, res: Response) => {
+
+        let nomebusca = req.params.nome;
+        let produto = produtoController.getProdutoByNome(nomebusca);
+        return res.json({produto});
+    })
+    
+    .post((req: Request, res: Response) => {
+        return res.json({Warning: "Método POST não suportado para a rota /:nome"});
+    })
+
+    .put((req: Request, res: Response) => {
+
+        let nome : string = req.params.nome;
+
+        let preco : number = parseInt(req.body.preco);
+        let quantidade : number = parseInt(req.body.quantidade);
+        let validade : Date = new Date(req.body.validade);
+
+        let updateProduto = produtoController.updateProduto(nome, preco, quantidade, validade);
+
+        switch(updateProduto) { 
+            case 1: { 
+                return res.status(404).json({Message: "Erro. Usuário não encontrado."});
+            } 
+            case 2: { 
+                return res.status(409).json({Message: "Erro. Data de validade inválida."});
+            } 
+            default: { 
+                return res.json({Message: "Produto atualizado com sucesso!"});
+            } 
+        } 
+
+    })
+
+    .delete((req: Request, res: Response) => {
+        let nome : string = req.params.nome;
+
+        let deleteProduto = produtoController.deleteProduto(nome);
+
+        if (!deleteProduto) {
+            return res.status(404).json({Message: "Erro. Produto não encontrado."});
+        }
+
+        return res.json({Message: "Produto removido com sucesso!"});
+    });
+
+
+
 
 export default produtoRouter;
